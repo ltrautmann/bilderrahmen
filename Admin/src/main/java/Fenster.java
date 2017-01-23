228e38d3bb1a2023e0597a43457d92a80351b0b8
you shall not pass <lt@lukas-trautmann.de>
2017-01-23 09:01:03 +0100

    TabPanel hinzugefügt

--- Admin/src/main/java/Fenster.java
+++ Admin/src/main/java/Fenster.java
@@ -8,18 +8,23 @@ import java.awt.Container;
  */
 public class Fenster extends JFrame {
     private Container c;
-
-
-    private P_ClientGroupSelect P_ClientGroupSelect;
+    private JTabbedPane tabbedPane;
+    private P_ClientGroupSelect p_ClientGroupSelect;
 
     public Fenster() {
-        c = getContentPane();
-        P_ClientGroupSelect = new P_ClientGroupSelect(true, null, null);
-
-        add(P_ClientGroupSelect);
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        initComponents();
         pack();
         setVisible(true);
+    }
+
+    private void initComponents(){
+        c = getContentPane();
+        tabbedPane = new JTabbedPane();
+        p_ClientGroupSelect = new P_ClientGroupSelect(true, null, null);
+        tabbedPane.addTab("Clients", null, p_ClientGroupSelect);
+        tabbedPane.addTab("Gruppen", null, new JPanel());
+        c.add(tabbedPane);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
     public static void main(String[] args) {
