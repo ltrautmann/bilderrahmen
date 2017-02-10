820a7c55f226c9374ea83c1dba84abf85b791ce1
you shall not pass <you shall not pass>
2017-02-10 10:48:08 +0100

    Initialer Commit für CLient

---
+++ Client/src/com/sabel/bilderrahmen/client/test.java
@@ -0,0 +1,15 @@
+package com.sabel.bilderrahmen.client;
+
+import com.sabel.bilderrahmen.client.utils.WebService.FileDownloader;
+
+import java.io.IOException;
+
+/**
+ * Created by you shall not pass on 10.02.2017.
+ */
+public class test {
+    public static void main(String[] args) {
+        FileDownloader.createInstance("https://bilderrahmen.cheaterll.de/files/", "", "", "gbs", "Kennwort0");
+        FileDownloader fd = FileDownloader.getInstance();
+    }
+}
