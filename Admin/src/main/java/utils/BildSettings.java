a330b2ed882118125d903088c15e70f58926063a
robin <robin.kernel@hotmail.de>
2017-02-10 14:08:08 +0100

    Gruppen Client zuordnung

---
+++ Admin/src/main/java/utils/BildSettings.java
@@ -0,0 +1,52 @@
+package utils;
+
+/**
+ * Created by Robin on 10.02.2017.
+ */
+public class BildSettings implements Bild {
+    private int dauerInSec;
+    private String location;
+
+    public BildSettings(String location, int dauerInSec) {
+        this.location = location;
+        this.dauerInSec = dauerInSec;
+    }
+
+
+    public int getDauerInSec() {
+        return dauerInSec;
+    }
+
+    public void setDauerInSec(int dauerInSec) {
+        this.dauerInSec = dauerInSec;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        BildSettings that = (BildSettings) o;
+
+      //  if (dauerInSec != that.dauerInSec) return false;
+        return location.equals(that.location);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = dauerInSec;
+        result = 31 * result + location.hashCode();
+        return result;
+    }
+
+    @Override
+    public String getLocation() {
+        return location;
+    }
+
+    @Override
+    public String getName() {
+        return location.substring(location.lastIndexOf('/'), location.length());
+    }
+}
