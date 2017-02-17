5e451a5bbc35e03cc35bb520bf145a75d3a5f37a
you shall not pass <you shall not pass>
2017-02-17 10:20:04 +0100

    Klasse ImageTools hinzugefügt Bildgröße anpassen

---
+++ Client/src/com/sabel/bilderrahmen/client/utils/ImageTools.java
@@ -0,0 +1,80 @@
+package com.sabel.bilderrahmen.client.utils;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+/**
+ * Created by you shall not pass on 17.02.2017.
+ */
+public class ImageTools {
+    public BufferedImage resizeImage(BufferedImage image) {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int width = (int) screenSize.getWidth();
+        int height = (int) screenSize.getHeight();
+        //adjust the target height or width to conserve the aspect ratio of the original image
+        double aspectRatio = image.getWidth() / image.getHeight();
+        if (width * aspectRatio < height) {
+            height = (int) (width * aspectRatio);
+        } else if (width * aspectRatio > height) {
+            width = (int) (height * aspectRatio);
+        }
+        return getScaledInstance(image, width, height, RenderingHints.VALUE_INTERPOLATION_BILINEAR, true);
+    }
+
+    /**
+     * Convenience method that returns a scaled instance of the
+     * provided {@code BufferedImage}.
+     * @param img the original image to be scaled
+     * @param targetWidth the desired width of the scaled instance, in pixels
+     * @param targetHeight the desired height of the scaled instance,  in pixels
+     * @param hint one of the rendering hints that corresponds to
+     *                      {@code RenderingHints.KEY_INTERPOLATION} (e.g.
+     *                      {@code RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR},
+     *                      {@code RenderingHints.VALUE_INTERPOLATION_BILINEAR},
+     *                      {@code RenderingHints.VALUE_INTERPOLATION_BICUBIC})
+     * @param higherQuality if true, this method will use a multi-step
+     *                      scaling technique that provides higher quality than the usual
+     *                      one-step technique (only useful in downscaling cases, where
+     *                      {@code targetWidth} or {@code targetHeight} is
+     *                      smaller than the original dimensions, and generally only when
+     *                      the {@code BILINEAR} hint is specified)
+     * @return a scaled version of the original {@code BufferedImage}
+     */
+    private BufferedImage getScaledInstance(BufferedImage img, int targetWidth, int targetHeight, Object hint, boolean higherQuality) {
+        int type = (img.getTransparency() == Transparency.OPAQUE) ? BufferedImage.TYPE_INT_RGB : BufferedImage.TYPE_INT_ARGB;
+        BufferedImage ret = (BufferedImage) img;
+        int w, h;
+        if (higherQuality) {
+            // Use multi-step technique: start with original size, then
+            // scale down in multiple passes with drawImage() // until the target size is reached
+            w = img.getWidth();
+            h = img.getHeight();
+        } else {
+            // Use one-step technique: scale directly from original
+            // size to target size with a single drawImage() call
+            w = targetWidth;
+            h = targetHeight;
+        }
+        do {
+            if (higherQuality && w > targetWidth) {
+                w /= 2;
+                if (w < targetWidth) {
+                    w = targetWidth;
+                }
+            }
+            if (higherQuality && h > targetHeight) {
+                h /= 2;
+                if (h < targetHeight) {
+                    h = targetHeight;
+                }
+            }
+            BufferedImage tmp = new BufferedImage(w, h, type);
+            Graphics2D g2 = tmp.createGraphics();
+            g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, hint);
+            g2.drawImage(ret, 0, 0, w, h, null);
+            g2.dispose();
+            ret = tmp;
+        } while (w != targetWidth || h != targetHeight);
+        return ret;
+    }
+}
