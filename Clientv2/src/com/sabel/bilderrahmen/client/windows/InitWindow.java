6832762f2682565c91125fa8fb3fc144cada4a5f
CheaterLL <lt@lukas-trautmann.de>
2017-04-06 21:07:45 +0200

    .

--- Clientv2/src/com/sabel/bilderrahmen/client/windows/InitWindow.java
+++ Clientv2/src/com/sabel/bilderrahmen/client/windows/InitWindow.java
@@ -1,11 +1,11 @@
 package com.sabel.bilderrahmen.client.windows;
 
 import javax.swing.JFrame;
+import java.awt.Component;
 
 /**
  * Created by CheaterLL on 05.04.2017.
  */
 public class InitWindow extends JFrame {
-
-
+    private Component c;
 }
