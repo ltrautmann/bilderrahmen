ebbbf7203228183c329a10a3129bd7dfec69af0d
robin <robin.kernel@hotmail.com>
2017-05-11 15:36:27 +0200

    Refresh projekt

---
+++ src/main/java/com/sabel/bilderrahmen/Admin/panels/ClientDeletePane.java
@@ -0,0 +1,10 @@
+package com.sabel.bilderrahmen.Admin.panels;
+
+import javax.swing.*;
+
+/**
+ * Created by robin on 11.05.17.
+ */
+public class ClientDeletePane extends JPanel {
+    private JTable table;
+}
