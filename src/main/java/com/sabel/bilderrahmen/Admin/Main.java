fd1846e5fafa69bc14f0347ce20a37d106eecded
robin <robin.kernel@hotmail.com>
2017-05-11 20:03:55 +0200

    Optische änderrungen

--- src/main/java/com/sabel/bilderrahmen/Admin/Main.java
+++ src/main/java/com/sabel/bilderrahmen/Admin/Main.java
@@ -5,6 +5,8 @@ import com.sabel.bilderrahmen.Admin.panels.MainWindow;
 import com.sabel.bilderrahmen.Admin.services.FileService;
 import com.sabel.bilderrahmen.Admin.services.FtpService;
 
+import javax.swing.*;
+
 
 /**
  * Created by robin on 18.04.17.
@@ -18,6 +20,17 @@ public class Main {
         FileService.readPictures();
         FtpService.getInstance().disconnect();
 
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
         new MainWindow();
 
     }
