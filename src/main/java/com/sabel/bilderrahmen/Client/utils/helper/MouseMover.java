6bd62d31a05867e912bbd246d9fc81d4e501eb93
you shall not pass <you shall not pass>
2017-05-15 14:46:32 +0200

    Robot now called WallE

--- src/main/java/com/sabel/bilderrahmen/Client/utils/helper/MouseMover.java
+++ src/main/java/com/sabel/bilderrahmen/Client/utils/helper/MouseMover.java
@@ -6,12 +6,12 @@ import java.awt.*;
  * Created by Robin on 10.05.2017.
  */
 public class MouseMover implements Runnable {
-    private static Robot hal;
+    private static Robot WallE;
 
     static {
         try {
-            hal = new Robot();
-            hal.mouseMove((int) Toolkit.getDefaultToolkit().getScreenSize().getWidth(),(int) Toolkit.getDefaultToolkit().getScreenSize().getHeight());
+            WallE = new Robot();
+            WallE.mouseMove((int) Toolkit.getDefaultToolkit().getScreenSize().getWidth(),(int) Toolkit.getDefaultToolkit().getScreenSize().getHeight());
         } catch (AWTException e) {
             e.printStackTrace();
         }
@@ -20,10 +20,10 @@ public class MouseMover implements Runnable {
 
     public static void runit() {
         while (!Thread.currentThread().isInterrupted()) {
-            hal.delay(1000);
+            WallE.delay(1000);
             int x = MouseInfo.getPointerInfo().getLocation().x ^ 1; // + - 1 Pixel
             int y = MouseInfo.getPointerInfo().getLocation().y ^ 1; // + - 1 Pixel
-            hal.mouseMove(x, y);
+            WallE.mouseMove(x, y);
         }
     }
 
